d91931a8b9deb78dbcf232864e041a60e60acf4c
Wildan Luqmanul Hakim <wildanluqmanul@gmail.com>
2023-12-03 22:08:18 +0700

    update route and add admin route

--- src/routes/userRoute.js
+++ src/routes/userRoute.js
@@ -5,8 +5,9 @@ const {
     getUserById,
     updateUserById,
     deleteUserById,
+    userUploadPicture,
 } = require('../controllers/userController');
-const { authenticateToken } = require('../middleware/authMiddleware');
+const { authenticateToken } = require('../middlewares/authMiddleware');
 
 const router = express.Router();
 
@@ -15,5 +16,6 @@ router.get('/', authenticateToken, getAllUsers);
 router.get('/:id', authenticateToken, getUserById);
 router.put('/:id', authenticateToken, updateUserById);
 router.delete('/:id', authenticateToken, deleteUserById);
+router.post('/:id/picture', authenticateToken, userUploadPicture);
 
 module.exports = router;
