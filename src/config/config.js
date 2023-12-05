64e8e2ecf0690970fc897c3139c2da4d8264da5c
Wildan Luqmanul Hakim <wildanluqmanul@gmail.com>
2023-12-05 09:28:14 +0700

    update

--- src/config/config.js
+++ src/config/config.js
@@ -6,6 +6,8 @@ const config = {
   passDatabase: process.env.PASSWORD_DATABASE,
   databaseName: process.env.DATABASE_NAME,
   accessTokenSecret: process.env.ACCESS_TOKEN_SECRET,
+  projectIdGCP: process.env.PROJECT_ID_DCP,
+  bucketName: process.env.BUCKET_NAME,
 };
 
 module.exports = config;
