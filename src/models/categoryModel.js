81e8bee6c586893fe80bc4e53c46f789c92a67e6
Wildan Luqmanul Hakim <wildanluqmanul@gmail.com>
2023-12-06 19:25:01 +0700

    update category

--- src/models/categoryModel.js
+++ src/models/categoryModel.js
@@ -11,11 +11,8 @@ const Category = dbConnection.define(
             autoIncrement: true,
             primaryKey: true,
         },
-        detailCategoryId: {
-            type: DataTypes.INTEGER,
-        },
         categoryName: {
-            type: DataTypes.STRING,
+            type: DataTypes.ENUM('Musik dan Hiburan', 'Edukasi', 'Seniman', 'Teater dan Aktor'),
         },
     },
     {
@@ -40,17 +37,74 @@ const detailCategory = dbConnection.define(
     },
 );
 
+Category.hasMany(detailCategory, {
+    foreignKey: 'categoryId',
+    as: 'detailCategory',
+    onDelete: 'CASCADE',
+});
 detailCategory.belongsTo(Category, {
     foreignKey: 'categoryId',
     as: 'category',
 });
-Category.hasOne(detailCategory, {
-    foreignKey: 'categoryId',
-    as: 'detailCategory',
-});
+
+async function seedDatabase() {
+    const categoriesData = [
+        {
+            name: 'Musik dan Hiburan',
+            detail: [
+                'Penyanyi',
+                'Band (Berbagai genre)',
+                'Band Akustik',
+                'DJ',
+                'Komedian',
+                'Ilusionis atau Pesulap',
+                'Penari (Berbagai jenis tarian)',
+                'MC (Pembawa Acara)',
+                'Moderator',
+            ],
+        },
+        { name: 'Edukasi', detail: ['Narasumber / Speaker'] },
+        {
+            name: 'Seniman',
+            detail: [
+                'Seniman Lukisan',
+                'Seniman Patung',
+                'Fotografer',
+                'Seniman Graffiti',
+                'Karikaturis',
+                'Seni Pertunjukan Lainnya',
+            ],
+        },
+        {
+            name: 'Teater dan Aktor',
+            detail: ['Seniman Seni Pertunjukan (Circus, Fire - Dancing, dll.)', 'Seniman Pantomim'],
+        },
+    ];
+
+    // eslint-disable-next-line no-restricted-syntax
+    for (const categoryData of categoriesData) {
+        // eslint-disable-next-line no-await-in-loop
+        const [category, created] = await Category.findOrCreate({
+            where: { categoryName: categoryData.name },
+            defaults: { categoryName: categoryData.name },
+        });
+
+        if (created) {
+            // eslint-disable-next-line no-restricted-syntax
+            for (const detailName of categoryData.detail) {
+                // eslint-disable-next-line no-await-in-loop
+                await detailCategory.create({
+                    detailCategoryName: detailName,
+                    categoryId: category.categoryId,
+                });
+            }
+        }
+    }
+}
 
 module.exports = { Category, detailCategory };
 
 (async () => {
     await dbConnection.sync();
+    await seedDatabase();
 })();
